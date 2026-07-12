bc42e41cc212e07271acaeaa62cb6d9e4ff09047
preetam9091 <preetamsain91@gmail.com>
2026-07-12 17:05:46 +0530

    Time: 0 ms (100%), Space: 9.2 MB (23.21%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s.empty() || s[0] == '0') return 0;
+        
+        int n = s.length();
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1; 
+        
+        for (int i = 2; i <= n; ++i) {
+            if (s[i - 1] != '0') {
+                dp[i] += dp[i - 1];
+            }
+            
+            int twoDigit = stoi(s.substr(i - 2, 2));
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        
+        return dp[n];
+    }
+};
